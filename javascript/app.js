d0b24aa0215ff95d8c5ef71f40f5169645835a69
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2022-11-15 18:47:32 -0300

    Detectando e guardando os números negativos

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,12 @@
 let inputs = []
+let negativos = []
 
 for (let cont = 0; cont < 5; cont ++) {
     inputs[cont] = window.prompt('Digite um número: ')
+    if (inputs[cont] < 0){
+        negativos[cont] = inputs[cont]
+    }
 }
 
-console.log(inputs)
+alert(inputs)
+alert(negativos)
