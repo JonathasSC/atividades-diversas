3b68cf760e664a2b2ea62b0023a8093d22e0e44c
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2022-11-16 20:58:04 -0300

    update

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,14 @@
-let inputs = []
-let negativos = []
+var inputs = []
+var negativos = []
+var lista1 = window.document.getElementById('n1')
+var lista2 = window.document.getElementById('n2')
 
-for (let cont = 0; cont < 5; cont ++) {
+for (cont = 0; cont < 5; cont ++) {
     inputs[cont] = window.prompt('Digite um número: ')
     if (inputs[cont] < 0){
         negativos[cont] = inputs[cont]
     }
 }
 
-alert(inputs)
-alert(negativos)
+lista1.innerHTML = inputs
+lista2.innerHTML = negativos
