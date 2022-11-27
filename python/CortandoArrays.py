740b49f618c8b639891f22e9c002049b67e17586
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2022-11-27 16:12:41 -0300

    Desafio Hackerrank

---
+++ python/CortandoArrays.py
@@ -0,0 +1,8 @@
+def pickingNumbers(a):
+    metade = len(a) // 2
+    lista1 = []
+    for i in range(0,metade):
+        lista1.append(i)
+    print(lista1)
+
+pickingNumbers([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
